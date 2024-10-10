f036f7ba56f6de6f9fa070cba29d4630ebe43bc5
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-10-09 22:14:00 -0300

    Adicionando logica de login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroFrom
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
   form = LoginForms()
+
+  if request.method == 'POST':
+    form = LoginForms(request.POST)
+
+    if form.is_valid():
+      nome = form['nome_login'].value()
+      senha = form['senha'].value()
+
+    usuario = auth.authenticate(
+      request,
+      usename=nome,
+      password=senha
+    )
+    if usuario is not None:
+      auth.login(request, usuario)
+      return redirect('index')
+    else:
+      return redirect('index')
+
   return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
   form = CadastroFrom()
+
   if request.method == 'POST':
     form = CadastroFrom(request.POST)
     
